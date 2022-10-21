3f9cf122f69743a2209d0e27924d69111d7571c1
VTetiana <114221825+VTetiana@users.noreply.github.com>
2022-10-21 15:30:02 +0300

    Updated the time

--- src/script.js
+++ src/script.js
@@ -1,35 +1,3 @@
-let now = new Date();
-console.log(now);
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let hours = now.getHours();
-
-function zeroHours() {
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-}
-zeroHours();
-let minutes = now.getMinutes();
-function zeroMinutes() {
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-}
-zeroMinutes();
-let currentTime = document.querySelector("#time");
-currentTime.innerHTML = `${day} ${hours}:${minutes}`;
-
 function search(event) {
   event.preventDefault();
   let enteredCity = document.querySelector("#entered-city");
@@ -40,12 +8,38 @@ function search(event) {
   let city = enteredCity.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+  function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    let time = `${day} ${hours}:${minutes}`;
+    return time;
+  }
+
   function showTemperature(response) {
     let temperature = Math.round(response.data.main.temp);
     console.log(temperature);
 
     let cityTemp = document.querySelector("#temperature");
     cityTemp.innerHTML = `${temperature}`;
+    let dateTime = document.querySelector("#time");
+    dateTime.innerHTML = formatDate(response.data.dt * 1000);
     let weatherDescription = document.querySelector("#description");
     weatherDescription.innerHTML = response.data.weather[0].description;
     let windCurrent = Math.round(response.data.wind.speed);
